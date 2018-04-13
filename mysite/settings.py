e52c07e8462b46eabf83f98f3d4ed86eefe62df9
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2018-04-13 07:03:45 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES= [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
