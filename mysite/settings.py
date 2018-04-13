041ed08c6444f0a2fdc070571d59308805b988c4
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2018-04-13 07:04:23 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,7 +100,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
