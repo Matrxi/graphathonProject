25820fbb625428d7f783591548408dfa30cea7c3
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2018-04-13 08:17:46 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,6 +104,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
