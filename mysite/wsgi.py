ec7966433e33e641cf115175b0bfb34b38ea2827
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2018-04-13 08:11:25 +0530

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,5 +14,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-application = DjangoWhiteNoise(application)
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
